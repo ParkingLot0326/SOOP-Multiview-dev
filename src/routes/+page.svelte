a6171ec486f5aa15111e827519788dbba862657a
ParkingLot <parkingLot0326@gmail.com>
2025-01-25 15:25:07 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+  import Video from "./Video.svelte";
+
+  let name = $state("");
+  let greetMsg = $state("");
+
+  async function greet(event: Event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+
+<Video />
